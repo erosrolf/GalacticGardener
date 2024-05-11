638dcec739f206a24ccc41f057d66d0203fac0b9
erosrolf <Erosrolf@gmail.com>
2024-05-11 13:54:50 +0300

    added grap animation

--- Assets/PlayerAnimationReader.cs
+++ Assets/PlayerAnimationReader.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class PlayerAnimationReader : MonoBehaviour
 {
-    // public Animator playerAnimator;
-    // void OnEnable()
-    // {
-    //     CollideInspector.CollideWithTree += playerAnimator.SetTrigger
-    // }
+    private Animator _playerAnimator;
 
-    // void Play
+    void Awake()
+    {
+        _playerAnimator = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        CollideInspector.CollideWithTree += Grap;
+    }
+
+    void OnDisable()
+    {
+        CollideInspector.CollideWithTree -= Grap;
+    }
+
+    public void Grap()
+    {
+        StartCoroutine(GrapAndDisable());
+    }
+
+    private IEnumerator GrapAndDisable()
+    {
+        _playerAnimator.enabled = true;
+        Debug.Log("Grap");
+        yield return new WaitForSeconds(0.7f);
+        Debug.Log("Grap");
+        _playerAnimator.enabled = false;
+    }
+
 }
